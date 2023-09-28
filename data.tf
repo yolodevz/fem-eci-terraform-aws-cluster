e8c43bbd388545e34fbd83324aafc4c2702ca58b
Yolodev <96150994+yolodevz@users.noreply.github.com>
2023-09-28 02:03:28 +0100

    fix: remove service related code (#2)
    * fix: update organization name
    
    * fix: try with a string
    
    * fix: remove service related code

--- data.tf
+++ data.tf
@@ -73,8 +73,3 @@ data "aws_security_group" "this_private" {
     values = [data.aws_vpc.this.id]
   }
 }
-
-data "tfe_outputs" "tfe" {
-  organization = "<your-organization-name>"
-  workspace    = "fem-eci-tfe"
-}
